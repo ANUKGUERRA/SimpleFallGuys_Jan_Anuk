2a8ffefc14399d2717faf36752cbacf5c28bc63e
ANUKGUERRA <91182468+ANUKGUERRA@users.noreply.github.com>
2026-04-15 20:44:53 +0200

    jibgaoiu

--- SimpleFallGuys_Jan_Anuk/Assets/Scripts/NetworkUI.cs
+++ SimpleFallGuys_Jan_Anuk/Assets/Scripts/NetworkUI.cs
@@ -4,23 +4,32 @@ using UnityEngine;
 
 public class NetworkUI : MonoBehaviour
 {
+    private void Awake()
+    {
+        NetworkManager.Singleton.OnClientConnectedCallback += (ulong nosequees) =>
+        {
+            Debug.Log(nosequees);
+        };
+        
+    }
     string ip = "";
     private void OnGUI()
     {
         if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
         {
-            ip = GUI.TextField(new Rect(10, 10, 200, 30), ip);
+        }
 
-            if (GUI.Button(new Rect(10, 50, 100, 30), "Host"))
-            {
-                NetworkManager.Singleton.StartHost();
-            }
+        ip = GUI.TextField(new Rect(10, 10, 200, 30), ip);
 
-            if(GUI.Button(new Rect(10, 100, 100, 30), "Client"))
-            {
-                NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData.Address = ip;
-                NetworkManager.Singleton.StartClient();
-            }
+        if (GUI.Button(new Rect(10, 50, 100, 30), "Host"))
+        {
+            NetworkManager.Singleton.StartHost();
+        }
+
+        if (GUI.Button(new Rect(10, 100, 100, 30), "Client"))
+        {
+            NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData.Address = ip;
+            NetworkManager.Singleton.StartClient();
         }
     }
 }
