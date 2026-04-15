78f80d3d6ee5abcd179dec07f3c4fa7770e4fb1a
ANUKGUERRA <91182468+ANUKGUERRA@users.noreply.github.com>
2026-04-15 20:25:35 +0200

    Ip

---
+++ SimpleFallGuys_Jan_Anuk/Assets/Scripts/NetworkUI.cs
@@ -0,0 +1,26 @@
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using UnityEngine;
+
+public class NetworkUI : MonoBehaviour
+{
+    string ip = "";
+    private void OnGUI()
+    {
+        if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+        {
+            ip = GUI.TextField(new Rect(10, 10, 200, 30), ip);
+
+            if (GUI.Button(new Rect(10, 50, 100, 30), "Host"))
+            {
+                NetworkManager.Singleton.StartHost();
+            }
+
+            if(GUI.Button(new Rect(10, 100, 100, 30), "Client"))
+            {
+                NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData.Address = ip;
+                NetworkManager.Singleton.StartClient();
+            }
+        }
+    }
+}
