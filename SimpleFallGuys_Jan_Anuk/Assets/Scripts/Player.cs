333b732dfc9b0badd75c16ea541633ef638b52f0
JS1331 <jansarda2003@gmail.com>
2026-05-25 20:00:53 +0200

    FinishLine + Player

--- SimpleFallGuys_Jan_Anuk/Assets/Scripts/Player.cs
+++ SimpleFallGuys_Jan_Anuk/Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed = 5.0f;
+    [SerializeField] private int points = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,6 +19,22 @@ public class Player : MonoBehaviour
         if(other.gameObject.tag == "powerUp")
         {
             speed += 2.0f;
+        }
+        else if(other.gameObject.CompareTag("finishLine"))
+        {
+            if (!FinishLine.alreadyArrived)
+            {
+                points += 100;
+                FinishLine.alreadyArrived = true;
+            }
+            else
+            {
+                points += 50;
+            }
+        }
+        else if( other.gameObject.tag == "deathZone")
+        {
+            this.gameObject.SetActive(false);
         }
     }
 }
