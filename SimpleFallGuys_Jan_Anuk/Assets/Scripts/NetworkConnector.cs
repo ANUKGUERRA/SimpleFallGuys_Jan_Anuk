1b43dfed0c1f38daaa2af4fde8744f329ed4046c
ANUKGUERRA <anukguerra@gmail.com>
2026-06-05 13:28:49 +0200

    ConnectMenu

--- SimpleFallGuys_Jan_Anuk/Assets/Scripts/NetworkConnector.cs
+++ SimpleFallGuys_Jan_Anuk/Assets/Scripts/NetworkConnector.cs
@@ -2,9 +2,26 @@ using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NetworkConnector : MonoBehaviour
 {
+    [SerializeField] private string gameSceneName = "";
+    private void Start()
+    {
+        NetworkManager.Singleton.OnServerStarted += OnServerStarted;
+    }
+
+    private void OnServerStarted()
+    {
+        NetworkManager.Singleton.SceneManager.LoadScene(gameSceneName, LoadSceneMode.Single);
+    }
+
+    private void OnDisable()
+    {
+        if (NetworkManager.Singleton != null)
+            NetworkManager.Singleton.OnServerStarted -= OnServerStarted;
+    }
     public void Host()
     {
         NetworkManager.Singleton.StartHost();
